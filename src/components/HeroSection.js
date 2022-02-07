718ac7170546555b32e240586fbd73b3845403c4
wcwe <wcwe822@gmail.com>
2022-02-07 10:08:24 +0800

    background video uurchluv

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -5,7 +5,7 @@ import './HeroSection.css';
 function HeroSection() {
   return (
     <div className='hero-container'>
-      <video src='https://rr3---sn-hvcpjvh-0aol.googlevideo.com/videoplayback?expire=1644218035&ei=U3IAYteBHIXBsAL-_I2QDA&ip=139.162.234.54&id=o-AMFiD91E8dxmNwnqTCIi-7qpYycs-5DGLlcNFFHPk_y-&itag=137&aitags=133%2C134%2C135%2C136%2C137%2C160%2C242%2C243%2C244%2C247%2C248%2C278&source=youtube&requiressl=yes&vprv=1&mime=video%2Fmp4&ns=YBom0P5PbYhU_plKz3qc2TUG&gir=yes&clen=38964609&otfp=1&dur=112.028&lmt=1635737400228901&keepalive=yes&fexp=24001373,24007246&c=WEB&txp=6216224&n=28zlXfFpWU-WVLZTF&sparams=expire%2Cei%2Cip%2Cid%2Caitags%2Csource%2Crequiressl%2Cvprv%2Cmime%2Cns%2Cgir%2Cclen%2Cotfp%2Cdur%2Clmt&ratebypass=yes&sig=AOq0QJ8wRAIgZGRe7xPVVsonF_7g7b0lsyj1-YisCnRQ-RJEJFhgjksCIDqaTlZU5a-EY3ClS3sEMlGoJgJ_2dwbtf3-PyHKvQDu&redirect_counter=1&rm=sn-aiges77s&req_id=bf6970552434a3ee&cms_redirect=yes&ipbypass=yes&mh=8a&mip=202.126.88.130&mm=31&mn=sn-hvcpjvh-0aol&ms=au&mt=1644197577&mv=m&mvi=3&pl=24&lsparams=ipbypass,mh,mip,mm,mn,ms,mv,mvi,pl&lsig=AG3C_xAwRAIgQJ5Eln5djL9gpFuUsZoHsphpFfsbFLhz1DjYh72_oZoCIFmkd7rR47habtQ6pk5qdUpJN_Hf2wVlhxJHHd8erTey' autoPlay loop muted />
+      <video src='videos/video-1.mp4' autoPlay loop muted />
       {/* <image src="/img-1.jpg" alt="image" /> */}
       <div className='text1'>
       <h1>"ОЮУ ТОЛГОЙ”-Н ОРГИЛ ҮЕ ОЙРТСООР</h1>
